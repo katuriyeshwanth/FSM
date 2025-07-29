bf549eb03ec6c240200d551d9448e339fbfcf82b
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-29 23:45:32 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -26,3 +26,21 @@ state <= s0;
 end
 endcase
 endmodule
+
+//serial parity checker
+module parity(clk,x,z);
+input clk,x;
+output reg z;
+reg even_odd;
+parameter even=0,odd=1;
+always @(posedge clk)
+case (even_odd)
+even:begin z<= x?1:0;
+    even_odd <=x?odd:even;
+    end
+odd:begin z<= x?0:1;
+    even_odd <=x?even:odd;
+    end
+default:even_odd <= even;
+endcase
+endmodule 
