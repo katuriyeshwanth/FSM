cac8afe596940ad2e637a3a5f9a092cd48a9dae1
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-30 00:49:36 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -44,3 +44,35 @@ odd:begin z<= x?0:1;
 default:even_odd <= even;
 endcase
 endmodule 
+
+//seq detector
+module seq_detector(clk,rst,x,z);
+input clk,rst,x;
+output reg z;
+parameter s0=0,s1=1,s2=2,s3=3;
+reg [1:0]ps,ns;
+always @ (posedge clk or posedge rst)
+if(rst) ps<=s0;
+else ps<=ns;
+always @(ps, x) begin
+    z = 0; // default output
+    case (ps)
+      s0: begin
+        ns = (x == 0) ? s1 : s0;
+      end
+      s1: begin
+        ns = (x == 1) ? s2 : s1;
+      end
+      s2: begin
+        ns = (x == 1) ? s3 : s1;
+      end
+      s3: begin
+        if (x == 0) begin
+          #10 z = 1;      // Sequence 0110 detected
+          ns = s1;    // Overlap possible
+        end else
+          ns = s0;
+      end
+    endcase
+  end
+endmodule
